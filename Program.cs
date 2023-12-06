b1dfe233863fc3da0d26b8183617eca661d18bb2
nurikan <nuri.kann@outlook.com>
2023-12-06 23:04:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//sınıf geçme hesaplama
+
+
+Console.WriteLine("Lütfen vize notunuzu giriniz: ");
+double vize = double.Parse(Console.ReadLine());
+Console.WriteLine("Lütfen final notunuzu giriniz: ");
+double final = double.Parse(Console.ReadLine());
+
+double vizeYuzde = vize * 0.4;
+double finalYuzde = final * 0.6;
+
+double ortalama = vizeYuzde + finalYuzde;
+
+if (ortalama > 60)
+{
+    Console.WriteLine("Dersi geçtiniz");
+    Console.WriteLine($"Ders ortalamanız{ortalama}");
+}
+else if (ortalama == 60)
+{
+    Console.WriteLine($"Ders ortalamanız{ortalama}");
+    Console.WriteLine("Dersi geçtiniz");
+}
+else
+{
+    Console.WriteLine($"Ders ortalamanız{ortalama}");
+    Console.WriteLine("Dersi geçemediniz!");
+    return;
+}
